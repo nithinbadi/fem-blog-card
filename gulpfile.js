9ad2cca127850f924d7e2365a10cb9fc00f4f16d
nithinbadi <badinithin@gmail.com>
2023-12-31 15:15:19 +0530

    changes

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+const {src,dest,watch,series} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const browsersync = require('browser-sync').create();
+
+//scss task
+function scssTask(){
+    return src('app/scss/**/*.scss',{sourcemaps:true})
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(),cssnano()]))
+    .pipe(dest('dist',{sourcemaps:'.'})
+    );
+}
+
+//browser sync serve
+function browserSyncServe(cb){
+    browsersync.init({
+        server:{
+            baseDir:'.'
+        },
+        notify:{
+            style:{
+                top:'auto',
+                bottom:'0',
+            }
+        }
+});
+cb();
+}
+
+//browser sync reload
+function browserSyncReload(cb){
+    browsersync.reload();
+    cb();
+}
+
+//watch task
+function watchTask(){
+    watch('*.html',browserSyncReload);
+    watch('app/scss/**/*.scss',series(scssTask,browserSyncReload));
+}
+
+//default task when gulp runs on terminal command
+exports.default = series(scssTask,browserSyncServe,watchTask);
